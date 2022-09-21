ee5767a2b60a93731a2f026429bc8d30cbb4783e
yymin1022 <yymin1022@gmail.com>
2022-09-21 11:12:23 +0900

    Update Assignment1 : Added Function for Input

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -2,12 +2,37 @@
 
 using namespace std;
 
+void selMenu();
+
 int main() {
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(false);
 
+
+
     cout << "Hello, World!" << "\n";
-    
     return 0;
+}
+
+void selMenu(){
+    cout << "1. Insertion" << "\n";
+    cout << "2. Search" << "\n";
+    cout << "3. Sorting Option" << "\n";
+    cout << "4. Exit" << "\n";
+    cout << ">" << " ";
+
+    int selNum;
+    cin >> selNum;
+
+    switch(selNum){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
 }
