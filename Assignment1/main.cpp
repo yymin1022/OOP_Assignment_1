fe0fa942e93897e8a24e3dbb9f090049b30f8886
yymin1022 <yymin1022@gmail.com>
2022-09-21 11:10:28 +0900

    Initialized Assignment1

---
+++ Assignment1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    cout << "Hello, World!" << "\n";
+    
+    return 0;
+}
