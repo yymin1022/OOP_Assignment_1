a0286e9fa15bf449647cff42d9d1fc7cb0abd673
yymin1022 <yymin1022@gmail.com>
2022-09-21 12:29:04 +0900

    Update Assignment1 : Added Search Function Base Logic

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -117,6 +117,19 @@ void searchStudent(){
 
     int selNum;
     cin >> selNum;
+
+    switch(selNum){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+    }
 }
 
 void sortStudent(){
