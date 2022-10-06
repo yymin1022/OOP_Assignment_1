359fad15342baa85f4a829a638ab09e859b1f35f
yymin1022 <yymin1022@gmail.com>
2022-10-07 03:43:18 +0900

    Update Assignment1 : Moved Student Class from Main CPP

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,26 +1,7 @@
 #include <bits/stdc++.h>
+#include "Student.h"
 
 using namespace std;
-
-class Student{
-private:
-    string birthYear;
-    string dept;
-    string name;
-    string studentID;
-    string telNum;
-
-public:
-    Student(const string &name, const string &studentID, const string &birthYear, const string &dept, const string &telNum);
-
-    string getBirth(){return this->birthYear;};
-    string getDept(){return this->dept;};
-    string getName(){return this->name;};
-    string getID(){return this->studentID;};
-    string getTel(){return this->telNum;};
-
-    void printStudent();
-};
 
 bool checkDuplicate(const string&);
 bool compareStudentDept(Student, Student);
@@ -306,31 +287,4 @@ void selMenu(){
             isExit = true;
             break;
     }
-}
-
-void Student::printStudent(){
-    cout.width(16);
-    cout.fill(' ');
-    cout << this->name << " ";
-    cout.width(11);
-    cout.fill(' ');
-    cout << this->studentID << " ";
-    cout.width(20);
-    cout.fill(' ');
-    cout << this->dept << " ";
-    cout.width(12);
-    cout.fill(' ');
-    cout << this->birthYear << " ";
-    cout.width(12);
-    cout.fill(' ');
-    cout << this->telNum << "\n";
-}
-
-Student::Student(const string &name, const string &studentID, const string &birthYear, const string &dept,
-                 const string &telNum) {
-    this->birthYear = birthYear;
-    this->dept = dept;
-    this->name = name;
-    this->studentID = studentID;
-    this->telNum = telNum;
 }
