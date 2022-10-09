e16e6a68d7704c09e3efd556ae3664b1fd0910ff
yymin1022 <yymin1022@gmail.com>
2022-10-10 02:15:50 +0900

    Update Asmt1 Prob1 : Added Functions on UIManager

--- Assignment1/Problem1/UIManager.h
+++ Assignment1/Problem1/UIManager.h
@@ -9,8 +9,24 @@ void showGreeting(){
     // This Function is for Showing Greeting Menu.
 }
 
-void showMenu(){
+void showMainMenu(){
     // This Function is for Showing Selection Menu of Main.
+}
+
+void showPlanMakeMenu(){
+    // This Function is for Showing Selection Menu of Plan Making.
+}
+
+void showPlanReviewMenu(){
+    // This Function is for Showing Selection Menu of Plan Reviewing.
+}
+
+void showRecipeBrowseMenu(){
+    // This Function is for Showing Selection Menu of Recipe Browsing.
+}
+
+void showRecipeAddEditMenu(){
+    // This Function is for Showing Selection Menu of Recipe Adding or Editing.
 }
 
 void clearScreen(){
