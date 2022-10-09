6a050a298426a6f0d59fba815567d53926baeb25
yymin1022 <yymin1022@gmail.com>
2022-10-10 02:06:17 +0900

    Update Asmt1 Prob1 : Added Functions on Date

--- Assignment1/Problem1/Date.h
+++ Assignment1/Problem1/Date.h
@@ -5,4 +5,42 @@
 
 using namespace std;
 
+class Date{
+private:
+    int year;
+    int month;
+    int day;
+
+public:
+    Date(int year, int month, int day){
+        this->year = year;
+        this->month = month;
+        this->day = day;
+    }
+
+    int getDay(){
+        // This Function is Day Getter.
+    }
+
+    int getMonth(){
+        // This Function is Month Getter.
+    }
+
+    int getYear(){
+        // This Function is Year Getter.
+    }
+
+    void setDay(int day){
+        // This Function is Day Setter.
+    }
+
+    void setMonth(int month){
+        // This Function is Month Setter.
+    }
+
+    void setYear(int year){
+        // This Function is Year Setter.
+    }
+};
+
 #endif
